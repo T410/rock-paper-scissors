a3ada56d8afc455e3155cb6d09d00724e426c81e
Tayyib Cankat <tayyib.cankat@adcolony.com>
2021-12-05 20:38:45 +0300

    Moving is now more mathematical. Items are moving to the closest targets

--- src/move.ts
+++ src/move.ts
@@ -39,7 +39,8 @@ function getNeighbours(item: Item, items: Item[], range: number) {
 
 	return neighbours;
 }
-
+let i = 3;
+let now = Date.now() - 1000;
 function moveTowards(source: Item, target: Item, v: number, hitbox: number): Item {
 	const { x: sourceX, y: sourceY } = source;
 	const { x: targetX, y: targetY } = target;
