f7457189aad290073310b886690800541409ed93
Tayyib Cankat <tayyib.cankat@adcolony.com>
2021-12-06 00:32:05 +0300

    Basic UI

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { RenderOptions } from "./models";
 import render from "./render";
+import "./style.css";
+
 function init() {
 	const scale = 0.5;
 	const WIDTH = 1920 * scale;
-	const HEIGHT = 1080 * scale;
+	const HEIGHT = window.visualViewport.height * scale * 0.9;
 
 	const canvas = document.querySelector("canvas");
 
 	function prepareCanvas(canvas: HTMLCanvasElement) {
 		canvas.width = WIDTH;
 		canvas.height = HEIGHT;
-		canvas.style.width = "100%";
-		canvas.style.height = "100%";
 	}
 
 	prepareCanvas(canvas);
@@ -22,7 +22,7 @@ function init() {
 		range: 500,
 		speed: 2,
 		FPS: 60,
-		hitbox: 50,
+		hitbox: 25,
 		drawHitbox: false,
 	};
 	render(renderOptions);
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = function (env) {
 				},
 				{
 					test: /\.css$/,
-					use: ["css-loader"],
+					use: ["style-loader", "css-loader"],
 				},
 				{
 					test: /\.(html)$/,
